deb3ac270e2435c574cd09771fd91b2766b0d225
Justin Terry <justin.terry@students.csulb.edu>
2019-08-28 16:28:54 -0700

    Day 1 of 30 Days of Code

--- Day 1/challenge1.java
+++ Day 1/challenge1.java
@@ -3,23 +3,35 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
+
 public class Solution {
-	public static void main(String[] args) {
-        // Create a Scanner object to read input from stdin.
-		Scanner scan = new Scanner(System.in); 
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
 		
-		// Read a full line of input from stdin and save it to our variable, inputString.
-		String inputString = scan.nextLine(); 
+        Scanner scan = new Scanner(System.in);
 
-		// Close the scanner object, because we've finished reading 
-        // all of the input from stdin needed for this challenge.
-		scan.close(); 
-      
-		// Print a string literal saying "Hello, World." to stdout.
-		System.out.println("Hello, World.");
-      
-	    // TODO: Write a line of code here that prints the contents of inputString to stdout.
-        System.out.println(inputString);
-	}
-}
+        /* Declare second integer, double, and String variables. */
+        int myInt;
+        double myDouble;
+        String myString;
 
+        /* Read and save an integer, double, and String to your variables.*/
+        myInt = scan.nextInt();
+        myDouble = scan.nextDouble();
+		// Clear the rest of the line left after scan.nextDouble()
+        scan.nextLine();
+        myString = scan.nextLine();
+
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(myInt + i);
+        /* Print the sum of the double variables on a new line. */
+		System.out.println(myDouble + d);
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+        System.out.println(s + myString);
+        scan.close();
