2bc852714f7a601b0c78db5b7e11558e67bc32aa
Justin Terry <Justin.Terry@student.csulb.edu>
2019-09-23 10:01:20 -0700

    Day 27

---
+++ Day 27/Day 27 Java Solution.java
@@ -0,0 +1,135 @@
+import java.util.*;
+
+public class Solution {
+
+    public static int minimum_index(int[] seq) {
+        if (seq.length == 0) {
+            throw new IllegalArgumentException("Cannot get the minimum value index from an empty sequence");
+        }
+        int min_idx = 0;
+        for (int i = 1; i < seq.length; ++i) {
+            if (seq[i] < seq[min_idx]) {
+                min_idx = i;
+            }
+        }
+        return min_idx;
+    }
+
+    static class TestDataEmptyArray {
+        public static int[] get_array() {
+            // complete this function
+            int[] empty = {};
+            return empty;
+        }
+    }
+
+    static class TestDataUniqueValues {
+        private static int minIndex = 0;
+
+        public static int[] get_array() {
+            // complete this function
+            int random = (int) Math.random() * 100 + 2;
+            ArrayList<Integer> ints = new ArrayList();
+            for(int i = 0; i < random; i++){
+                ints.add(i);
+            }
+            Collections.shuffle(ints);
+            int[] arr = new int[random];
+            for(int i = 0; i < random; i++){
+                arr[i] = ints.get(i);
+            }
+            minIndex = ints.indexOf(Collections.min(ints));
+            return arr;
+        }
+
+        public static int get_expected_result() {
+            // complete this function
+            return minIndex;
+        }
+    }
+
+    static class TestDataExactlyTwoDifferentMinimums {
+        private static int minIndex;
+
+        public static int[] get_array() {
+            int random = (int) Math.random() * 100 + 2;
+            ArrayList<Integer> ints = new ArrayList();
+            for(int i = 0; i < random; i++){
+                ints.add(i);
+                if(i == 0) {
+                    ints.add(i);
+                }
+            }
+            Collections.shuffle(ints);
+            int[] arr = new int[random];
+            for(int i = 0; i < random; i++){
+                arr[i] = ints.get(i);
+            }
+            minIndex = ints.indexOf(Collections.min(ints));
+            return arr;
+        }
+
+        public static int get_expected_result() {
+            // complete this function
+            return minIndex;
+        }
+    }
+
+    
+	public static void TestWithEmptyArray() {
+        try {
+            int[] seq = TestDataEmptyArray.get_array();
+            int result = minimum_index(seq);
+        } catch (IllegalArgumentException e) {
+            return;
+        }
+        throw new AssertionError("Exception wasn't thrown as expected");
+    }
+
+    public static void TestWithUniqueValues() {
+        int[] seq = TestDataUniqueValues.get_array();
+        if (seq.length < 2) {
+            throw new AssertionError("less than 2 elements in the array");
+        }
+
+        Integer[] tmp = new Integer[seq.length];
+        for (int i = 0; i < seq.length; ++i) {
+            tmp[i] = Integer.valueOf(seq[i]);
+        }
+        if (!((new LinkedHashSet<Integer>(Arrays.asList(tmp))).size() == seq.length)) {
+            throw new AssertionError("not all values are unique");
+        }
+
+        int expected_result = TestDataUniqueValues.get_expected_result();
+        int result = minimum_index(seq);
+        if (result != expected_result) {
+            throw new AssertionError("result is different than the expected result");
+        }
+    }
+
+    public static void TestWithExactlyTwoDifferentMinimums() {
+        int[] seq = TestDataExactlyTwoDifferentMinimums.get_array();
+        if (seq.length < 2) {
+            throw new AssertionError("less than 2 elements in the array");
+        }
+
+        int[] tmp = seq.clone();
+        Arrays.sort(tmp);
+        if (!(tmp[0] == tmp[1] && (tmp.length == 2 || tmp[1] < tmp[2]))) {
+            throw new AssertionError("there are not exactly two minimums in the array");
+        }
+
+        int expected_result = TestDataExactlyTwoDifferentMinimums.get_expected_result();
+        int result = minimum_index(seq);
+        if (result != expected_result) {
+            throw new AssertionError("result is different than the expected result");
+        }
+    }
+
+    public static void main(String[] args) {
+        TestWithEmptyArray();
+        TestWithUniqueValues();
+        TestWithExactlyTwoDifferentMinimums();
+        System.out.println("OK");
+    }
+}
